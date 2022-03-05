8b374f2ad679db07c9f6ebfeaaee3eb5684504ab
Yoji Ito <yoji@YojiMBP2021.local>
2022-03-05 17:26:48 +0900

    todo in memomry

--- main.go
+++ main.go
@@ -1,12 +1,59 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
 
-func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+var todos []TODOObject = []TODOObject{}
+
+type MetaData struct {
+	ID        int       `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, world!"))
+type TODO struct {
+	Author      string    `json:"author"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	DueDate     time.Time `json:"dueDate"`
+}
+
+type TODOObject struct {
+	MetaData
+	TODO
+}
+
+func list(response http.ResponseWriter, request *http.Request) {
+	json.NewEncoder(response).Encode(todos)
+}
+
+func add(response http.ResponseWriter, request *http.Request) {
+	encoder := json.NewEncoder(response)
+	if request.Body == http.NoBody {
+		encoder.Encode("request body is nil")
+		return
+	}
+	var todo TODO
+	if err := json.NewDecoder(request.Body).Decode(&todo); err != nil {
+		encoder.Encode(err.Error())
+		return
+	}
+	metadata := MetaData{
+		ID:        len(todos) + 1,
+		CreatedAt: time.Now(),
+	}
+	object := TODOObject{
+		TODO:     todo,
+		MetaData: metadata,
+	}
+	todos = append(todos, object)
+	encoder.Encode(object)
+}
+
+func main() {
+	http.HandleFunc("/todo/list", list)
+	http.HandleFunc("/todo/add", add)
+	http.ListenAndServe(":8080", nil)
 }
